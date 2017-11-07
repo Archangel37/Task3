06dc05957675fc41417748c255f4070b70ab0ab9
Archangel37 <djarchangel3737@gmail.com>
2017-11-07 12:15:47 +0300

    code styling + fixed exeption

---
+++ Task3_GenericVector/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Task3_GenericVector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Evaluate = new System.Windows.Forms.Button();
+            this.richTextBox_result = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_v1 = new System.Windows.Forms.TextBox();
+            this.textBox_v2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Evaluate
+            // 
+            this.button_Evaluate.Location = new System.Drawing.Point(572, 36);
+            this.button_Evaluate.Name = "button_Evaluate";
+            this.button_Evaluate.Size = new System.Drawing.Size(75, 23);
+            this.button_Evaluate.TabIndex = 0;
+            this.button_Evaluate.Text = "Evaluate";
+            this.button_Evaluate.UseVisualStyleBackColor = true;
+            this.button_Evaluate.Click += new System.EventHandler(this.button_Evaluate_Click);
+            // 
+            // richTextBox_result
+            // 
+            this.richTextBox_result.Location = new System.Drawing.Point(12, 150);
+            this.richTextBox_result.Name = "richTextBox_result";
+            this.richTextBox_result.Size = new System.Drawing.Size(784, 427);
+            this.richTextBox_result.TabIndex = 1;
+            this.richTextBox_result.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vector1: x(space)y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Vector2: x(space)y";
+            // 
+            // textBox_v1
+            // 
+            this.textBox_v1.Location = new System.Drawing.Point(125, 36);
+            this.textBox_v1.Name = "textBox_v1";
+            this.textBox_v1.Size = new System.Drawing.Size(276, 20);
+            this.textBox_v1.TabIndex = 3;
+            // 
+            // textBox_v2
+            // 
+            this.textBox_v2.Location = new System.Drawing.Point(124, 87);
+            this.textBox_v2.Name = "textBox_v2";
+            this.textBox_v2.Size = new System.Drawing.Size(276, 20);
+            this.textBox_v2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 589);
+            this.Controls.Add(this.textBox_v2);
+            this.Controls.Add(this.textBox_v1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox_result);
+            this.Controls.Add(this.button_Evaluate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Evaluate;
+        private System.Windows.Forms.RichTextBox richTextBox_result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_v1;
+        private System.Windows.Forms.TextBox textBox_v2;
+    }
+}
+
---
+++ Task3_GenericVector/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task3_GenericVector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Evaluate_Click(object sender, EventArgs e)
+        {
+            richTextBox_result.Text = string.Empty;
+            try
+            {
+                var vec1 = new double[2];
+                var vec2 = new double[2];
+
+                //тут не продумывал пока, если ввод будет с двумя пробелами или более 2х значений
+                //3е значение после пробела просто игнорируется
+                //потестил - обёртки try/catch для многого хватает, проблема возникает только в Convert.ToDouble
+                //ну и, собственно всё в try т.к. если не удалось сконвертировать, то и дальше нет смысла выполнять после мессадж-бокса
+                var split1 = textBox_v1.Text.Split(' ');
+                var split2 = textBox_v2.Text.Split(' ');
+
+                for (var i = 0; i < 2; i++)
+                {
+                    vec1[i] = Convert.ToDouble(split1[i]);
+                    vec2[i] = Convert.ToDouble(split2[i]);
+                }
+
+                var vector1 = new Vector(vec1[0], vec1[1]);
+                var vector2 = new Vector(vec2[0], vec2[1]);
+
+                richTextBox_result.Text += "Vector operations:" + Environment.NewLine;
+                richTextBox_result.Text += "Vector Summ: X(" + GenericVector<Vector, Vector>.Add(vector1, vector2).X +
+                                           ") Y(" + GenericVector<Vector, Vector>.Add(vector1, vector2).Y + ")" +
+                                           Environment.NewLine;
+                richTextBox_result.Text += "Vector Subtraction: X(" +
+                                           GenericVector<Vector, Vector>.Subtraction(vector1, vector2).X + ") Y(" +
+                                           GenericVector<Vector, Vector>.Subtraction(vector1, vector2).Y + ")" +
+                                           Environment.NewLine;
+                richTextBox_result.Text += "Compare Vectors: " +
+                                           GenericVector<Vector, Vector>.Compare(vector1, vector2) +
+                                           Environment.NewLine;
+            }
+            catch (FormatException ex)
+            {
+                var errorMessage = ex.Message + Environment.NewLine + Environment.NewLine
+                                   + ex.StackTrace + Environment.NewLine + Environment.NewLine
+                                   + ex.Source + Environment.NewLine
+                                   + ex.Data;
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK);
+                //тут чисто по-человечески жалко пользователя =))) , но очищаю поля, чтоб заново вводить при ошибочном вводе
+                textBox_v1.Text = string.Empty;
+                textBox_v2.Text = string.Empty;
+            }
+        }
+    }
+
+
+    //создадим класс для вектора
+    //посчитал, что все векторы начинаются в (0,0) и у них есть только координаты конца (х,у)
+    public class Vector
+    {
+        public double X;
+        public double Y;
+
+        //инициализация самого вектора
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        //определим операцию вычисления длины вектора, чтоб в дженерике можно было сравнить длины
+        public double Length => Math.Sqrt(X * X + Y * Y);
+
+        //определим оператор сложения двух векторов
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        //определим оператор вычитания двух векторов
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+    }
+
+    public class GenericVector<T,U>
+        where T : Vector
+        where U : Vector
+    {
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            var result = v1 + v2;
+            return result;
+        }
+
+        public static Vector Subtraction(Vector v1, Vector v2)
+        {
+            var result = v1 - v2;
+            return result;
+        }
+
+        public static string Compare(Vector v1, Vector v2)
+        {
+            var result = "";
+            if (v1.Length > v2.Length)
+                result = "length v1 > length v2";
+            if (v1.Length < v2.Length)
+                result = "length v1 < length v2";
+            if (v1.Length == v2.Length)
+                result = "length v1 = length v2";
+            return result;
+        }
+    }
+}
