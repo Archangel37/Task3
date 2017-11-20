f446c92ff6f24b10e4bd6698e6adb47b5f669542
Archangel37 <djarchangel3737@gmail.com>
2017-11-20 15:00:49 +0300

    fixed Generic Vector, used less code

--- Task3_GenericVector/Form1.cs
+++ Task3_GenericVector/Form1.cs
@@ -5,6 +5,8 @@ namespace Task3_GenericVector
 {
     public partial class Form1 : Form
     {
+        public const int Dimension = 2;
+
         public Form1()
         {
             InitializeComponent();
@@ -15,44 +17,19 @@ namespace Task3_GenericVector
             richTextBox_result.Text = string.Empty;
             try
             {
-                var vec1 = new double[2];
-                var vec2 = new double[2];
-
-                //тут не продумывал пока, если ввод будет с двумя пробелами или более 2х значений
-                //3е значение после пробела просто игнорируется
-                //потестил - обёртки try/catch для многого хватает, проблема возникает только в Convert.ToDouble
-                //ну и, собственно всё в try т.к. если не удалось сконвертировать, то и дальше нет смысла выполнять после мессадж-бокса
+                //FIXED - сократил запись
                 var split1 = textBox_v1.Text.Split(' ');
                 var split2 = textBox_v2.Text.Split(' ');
+                var vec1 = new Vector<double>(Convert.ToDouble(split1[0]), Convert.ToDouble(split1[1]));
+                var vec2 = new Vector<double>(Convert.ToDouble(split2[0]), Convert.ToDouble(split2[1]));
 
-                for (var i = 0; i < 2; i++)
-                {
-                    vec1[i] = Convert.ToDouble(split1[i]);
-                    vec2[i] = Convert.ToDouble(split2[i]);
-                }
-
-                var vector1 = new Vector(vec1[0], vec1[1]);
-                var vector2 = new Vector(vec2[0], vec2[1]);
-
-                richTextBox_result.Text += "Vector operations:" + Environment.NewLine;
-                richTextBox_result.Text += "Vector Summ: X(" + GenericVector<Vector, Vector>.Add(vector1, vector2).X +
-                                           ") Y(" + GenericVector<Vector, Vector>.Add(vector1, vector2).Y + ")" +
-                                           Environment.NewLine;
-                richTextBox_result.Text += "Vector Subtraction: X(" +
-                                           GenericVector<Vector, Vector>.Subtraction(vector1, vector2).X + ") Y(" +
-                                           GenericVector<Vector, Vector>.Subtraction(vector1, vector2).Y + ")" +
-                                           Environment.NewLine;
-                richTextBox_result.Text += "Compare Vectors: " +
-                                           GenericVector<Vector, Vector>.Compare(vector1, vector2) +
-                                           Environment.NewLine;
+                richTextBox_result.Text += Vector<double>.ToStringVec(vec1, vec2);
             }
             catch (FormatException ex)
             {
-                var errorMessage = ex.Message + Environment.NewLine + Environment.NewLine
-                                   + ex.StackTrace + Environment.NewLine + Environment.NewLine
-                                   + ex.Source + Environment.NewLine
-                                   + ex.Data;
-                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK);
+                //FIXED: -unnecessary strings, + MessageBoxIcon.Error
+                var errorMessage = ex.Message;
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 //тут чисто по-человечески жалко пользователя =))) , но очищаю поля, чтоб заново вводить при ошибочном вводе
                 textBox_v1.Text = string.Empty;
                 textBox_v2.Text = string.Empty;
@@ -60,63 +37,95 @@ namespace Task3_GenericVector
         }
     }
 
+    //3. Написать Generic Vector class, где типом может быть
+    //только value значение, надо реализовать операции + - и сравнения.
+    //В качестве сравнения использовать длину вектора.Вектор двухмерный. 
+
 
     //создадим класс для вектора
     //посчитал, что все векторы начинаются в (0,0) и у них есть только координаты конца (х,у)
-    public class Vector
+    //тут решарпер предлагает ещё Equals и GetHashCode, но для задачи вроде не нужно
+    public class Vector<T> 
     {
-        public double X;
-        public double Y;
+
+        //!!!!!!!!!!!!!!!!!!!!!
+        public ValueType X;
+        public ValueType Y;
 
         //инициализация самого вектора
-        public Vector(double x, double y)
+        public Vector(ValueType x, ValueType y)
         {
             X = x;
             Y = y;
         }
 
-        //определим операцию вычисления длины вектора, чтоб в дженерике можно было сравнить длины
-        public double Length => Math.Sqrt(X * X + Y * Y);
+        //определим операцию вычисления длины вектора
+        public double Length => Math.Sqrt((double)X * (double)X + (double)Y * (double)Y);
+
 
         //определим оператор сложения двух векторов
-        public static Vector operator +(Vector v1, Vector v2)
+        public static Vector<T> operator +(Vector<T> v1, Vector<T> v2)
         {
-            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+            return new Vector<T>((double)v1.X + (double)v2.X, (double)v1.Y + (double)v2.Y);
         }
 
         //определим оператор вычитания двух векторов
-        public static Vector operator -(Vector v1, Vector v2)
+        public static Vector<T> operator -(Vector<T> v1, Vector<T> v2)
         {
-            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+            return new Vector<T>((double)v1.X - (double)v2.X, (double)v1.Y - (double)v2.Y);
+        }
+
+
+        //!!!!!
+        public static bool operator <(Vector<T> v1, Vector<T> v2)
+        {
+            return v1.Length < v2.Length;
+        }
+
+        public static bool operator >(Vector<T> v1, Vector<T> v2)
+        {
+            return v1.Length > v2.Length;
+        }
+
+
+        public static bool operator <=(Vector<T> v1, Vector<T> v2)
+        {
+            return v1.Length <= v2.Length;
+        }
+
+        public static bool operator >=(Vector<T> v1, Vector<T> v2)
+        {
+            return v1.Length >= v2.Length;
+        }
+
+        public static bool operator ==(Vector<T> v1, Vector<T> v2)
+        {
+            return v1.Length == v2.Length;
+        }
+
+        public static bool operator !=(Vector<T> v1, Vector<T> v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static string CompareVectors(Vector<T> v1, Vector<T> v2)
+        {
+            if (v1 > v2)
+                return "v1 > v2";
+            if (v1 < v2)
+                return "v1 < v2";
+
+            return "v1 == v2";
+        }
+
+        public static string ToStringVec(Vector<T> v1, Vector<T> v2)
+        {
+            return "Vector operations:" + Environment.NewLine +
+                   "Vector Summ: X(" + (v1 + v2).X + ") Y(" + (v1 + v2).Y + ")" + Environment.NewLine +
+                   "Vector Subtraction: X(" + (v1 - v2).X + ") Y(" + (v1 - v2).Y + ")" + Environment.NewLine +
+                   "Compare Vectors: " + CompareVectors(v1, v2) + Environment.NewLine;
         }
     }
 
-    public class GenericVector<T,U>
-        where T : Vector
-        where U : Vector
-    {
-        public static Vector Add(Vector v1, Vector v2)
-        {
-            var result = v1 + v2;
-            return result;
-        }
-
-        public static Vector Subtraction(Vector v1, Vector v2)
-        {
-            var result = v1 - v2;
-            return result;
-        }
-
-        public static string Compare(Vector v1, Vector v2)
-        {
-            var result = "";
-            if (v1.Length > v2.Length)
-                result = "length v1 > length v2";
-            if (v1.Length < v2.Length)
-                result = "length v1 < length v2";
-            if (v1.Length == v2.Length)
-                result = "length v1 = length v2";
-            return result;
-        }
-    }
+   
 }
